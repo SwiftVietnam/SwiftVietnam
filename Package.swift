42b0d3ff86110e8b56cc7dba6668c0c40c68884c
An Tran <tran.binhan@gmail.com>
2020-08-27 13:32:24 +0800

    Update production

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .executable(name: "SwiftVietnam", targets: ["SwiftVietnam"])
     ],
     dependencies: [
-        .package(url: "https://github.com/johnsundell/publish.git", from: "0.3.0"),
+        .package(url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
         .package(url: "https://github.com/Ze0nC/SwiftPygmentsPublishPlugin", .branch("master"))
     ],
     targets: [
